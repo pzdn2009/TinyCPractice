c3955ec689d4136920140b753b66a0db018fd98b
pzdn2009 <1050244110@qq.com>
2016-09-02 22:04:01 +0800

    #add reserved lookup define and method

--- scan.c
+++ scan.c
@@ -7,6 +7,15 @@
 #include "globals.h"
 #include "scan.h"
 
+// 保留字查找表，用了静态结构体的赋值
+static struct {
+    char* str;
+    TokenType tok;
+} reservedWords[MAXRESERVED]
+= {{"if",IF},{"then",THEN},{"else",ELSE},{"end",END},
+   {"repeat",REPEAT},{"until",UNTIL},{"read",READ},
+   {"write",WRITE}};
+
 TokenType getToken(void)
 {
     
